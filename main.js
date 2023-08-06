256b9e4b15438481cc751530c1e0733291c8ebd4
Balazs Miskey <b.miskey2005@gmail.com>
2023-08-06 16:19:04 +0200

    created basic layout

---
+++ main.js
@@ -0,0 +1,30 @@
+let theme = 1;
+
+document.querySelector(':root').style.setProperty('--themeColor', localStorage.getItem("themeColorLocalStorage"));
+playVisibility();
+
+
+function showThemes() {
+	var x = document.getElementById("themes");
+	if (x.style.visibility == "visible") {
+		document.getElementById("palette-icon").style.color = 'var(--iconColor)';
+	  	x.style.visibility = "hidden";
+	} else {
+		document.getElementById("palette-icon").style.color = 'var(--themeColor)';
+	  	x.style.visibility = "visible";
+		
+	}
+}
+
+
+function updateThemeColor() {
+	localStorage.setItem("themeColorLocalStorage", getComputedStyle(document.documentElement).getPropertyValue('--themeColor'));
+	document.getElementById("palette-icon").style.color = 'var(--themeColor)';
+	document.getElementById("wave").style.color = 'var(--themeColor)';
+
+	localStorage.setItem("workLocalStorage",  document.getElementById("workInput").value);
+	localStorage.setItem("shortBreakLocalStorage", document.getElementById("shortBreakInput").value);
+	localStorage.setItem("longBreakLocalStorage", document.getElementById("longBreakInput").value);
+
+}
+
